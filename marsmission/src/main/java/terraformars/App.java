991f9f7bfc674fe052d7063ae49eaffabf631e79
Ray Oliveira <reinildes.jmo@gmail.com>
2020-08-06 23:56:57 -0300

    sketch code to read from user input

--- marsmission/src/main/java/terraformars/App.java
+++ marsmission/src/main/java/terraformars/App.java
@@ -2,9 +2,28 @@ package terraformars;
 
 import terraformars.directions.ProbeDirection;
 
+import java.util.Scanner;
+
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+
+        var scanner = new Scanner(System.in);
+
+        scanner.nextLine();
+
+        final var x = scanner.nextInt();
+        final var y = scanner.nextInt();
+
+        final var point = new Point(x, y);
+
+        System.out.println(point);
+
+        final var directionName = scanner.next();
+        final var probeDirection = ProbeDirection.create(directionName);
+
+        System.out.println(probeDirection);
+
+
     }
 
     public static class Probe {
