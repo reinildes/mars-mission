145b7c8d9e3015a44f179f78f3098847126dd5f0
Ray Oliveira <reinildes.jmo@gmail.com>
2020-08-06 22:22:01 -0300

    sketch modeling problem with classes

--- marsmission/src/main/java/terraformars/App.java
+++ marsmission/src/main/java/terraformars/App.java
@@ -1,13 +1,15 @@
 package terraformars;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+    }
+
+    public static class Probe {
+        public final ProbePosition probeState;
+
+        public Probe(ProbePosition probeState) {
+            this.probeState = probeState;
+        }
     }
 }
---
+++ marsmission/src/main/java/terraformars/ProbePosition.java
@@ -0,0 +1,7 @@
+package terraformars;
+
+public interface ProbePosition {
+    ProbePosition turnLeft();
+    ProbePosition turnRight();
+    Point move();
+}
