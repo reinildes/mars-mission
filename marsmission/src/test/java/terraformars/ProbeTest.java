78d3ec7ac56904dc861b5307a7cdb2f1ec8e2f55
Ray Oliveira <reinildes.jmo@gmail.com>
2020-08-07 01:56:13 -0300

    add more tests for edge cases

--- marsmission/src/test/java/terraformars/ProbeTest.java
+++ marsmission/src/test/java/terraformars/ProbeTest.java
@@ -8,10 +8,32 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class ProbeTest {
+
     @Test
-    public void probeExecutesCommands() {
+    public void probeExecutesCommandsA() {
         final var probe = new Probe(ProbeDirection.create("N"), new Point(1, 2), "LMLMLMLMM");
         probe.executeCommands();
         assertThat(probe.getPosition(), is("1 3 N"));
+    }
+
+    @Test
+    public void probeExecutesCommandsB() {
+        final var probe = new Probe(ProbeDirection.create("E"), new Point(3, 3), "MMRMMRMRRM");
+        probe.executeCommands();
+        assertThat(probe.getPosition(), is("5 1 E"));
+    }
+
+    @Test
+    public void probeExecutesCommandsWithNegativeNumbers() {
+        final var probe = new Probe(ProbeDirection.create("E"), new Point(-3, -3), "MMRMMRMRRM");
+        probe.executeCommands();
+        assertThat(probe.getPosition(), is("-1 -5 E"));
+    }
+
+    @Test
+    public void probeExecutesCommandsWithBigCommands() {
+        final var probe = new Probe(ProbeDirection.create("E"), new Point(1000, 1000), "LLMRLMRLRRLLMMLMRLMRMLRMLRMLRMLRMLLMMRRLM");
+        probe.executeCommands();
+        assertThat(probe.getPosition(), is("993 997 S"));
     }
 }
